0ec3b8f5615186069bfc21968fc2a368646db1b3
quinjenn <jquintal_@hotmail.com>
2023-01-19 09:30:25 -0800

    adding updates and input.js

---
+++ input.js
@@ -0,0 +1,23 @@
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin; // event listener for sdin object
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // event listener for stdin
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+// data is received from stdin // particular key is pressed on the keyboard input
+const handleUserInput = (pressedKey) => {};
+
+/* Supported Move Commands
+"Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left)*/
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,28 +1,7 @@
-const net = require("net");
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin; // event listener for sdin object
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  // event listener for stdin
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-// data is received from stdin // particular key is pressed on the keyboard input
-const handleUserInput = (pressedKey) => {};
-
-/* Supported Move Commands
-"Move: up" - move up one square (unless facing down)
-"Move: down" - move down one square (unless facing up)
-"Move: left" - move left one square (unless facing right)
-"Move: right" - move left one square (unless facing left)*/
+setupInput();
