e5957f6a102f0ed37d396944a6edbcd5d0a62747
quinjenn <jquintal_@hotmail.com>
2023-01-19 09:07:46 -0800

    updated move commands

--- play.js
+++ play.js
@@ -3,3 +3,9 @@ const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+/* Supported Move Commands
+"Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left)*/
